e5d767f8e7baafae124e01bb149b9d4a35427197
Oliver Steele <steele@osteele.com>
2018-01-22 09:29:45 -0500

    Initial

---
+++ README.rst
@@ -0,0 +1,93 @@
+Jupyter Secrets
+================
+
+|PyPI version| |Doc Status| |License| |Supported Python|
+
+**Jupyter Secrets** provides functions for using secrets in a Jupyter notebook.
+
+The first time :func:`get_secret` is called, it prompts the user for the value.
+After this value is read, it is saved in keypass, and the Jupyter output cell is
+cleared.
+
+|gif1|
+
+Subsequent calls to :func:`get_secret`, use the saved value. This is true even
+if they are called in a different process running in the same account -- for
+example, if the machine has been rebooted, or the local Jupyter server has
+otherwise been shut down and restarted.
+
+|gif2|
+
+.. note:: This package won't work on Colaboratory_ (Colaboratory doesn't run
+    `keyring`). It probably won't work on some other hosted notebook environments.
+
+.. warning:: This package stores the secret on the Jupyter server. Don't trust
+    it with secrets you don't trust the Jupyter server with. (This is true for
+    all means of using a secret in a notebook.)
+
+.. warning:: The package is intended to reduce the likelihood of accidental
+    disclosure of secrets in notebook source. It won't secure a secret from code
+    that is running *in* the notebook; and it won't keep you from writing code
+    that displays the secret in a notebook output cell -- in which case it has
+    been disclosed to whoever can see the notebook.
+
+Usage
+-----
+
+.. code:: python
+
+        from jupyter_secrets import *
+
+        TWILIO_API_KEY = get_secret('TWILIO_API_KEY')
+
+Alternatives
+------------
+
+Secrets can also be stored in an environment variable, and read from the
+notebook. This is a best practice for applications (and especially web and other
+`server-side services`_), but I've found it inconvenient for notebooks -- the
+notebook server must be re-started to pick up a new environment variable; and,
+it complicates the setup instructions for notebook users.
+
+Development
+-----------
+
+Install dev tools:
+
+.. code:: bash
+
+    $ pip install -r requirements.txt
+
+Install locally:
+
+.. code:: bash
+
+    flit install --symlink
+
+License
+-------
+
+MIT
+
+.. |PyPI version| image:: https://img.shields.io/pypi/v/jupyter_secrets.svg
+    :target: https://pypi.python.org/pypi/jupyter_secrets
+    :alt: Latest PyPI Version
+.. |Doc Status| image:: https://readthedocs.org/projects/jupyter_secrets/badge/?version=latest
+    :target: http://jupyter_secrets.readthedocs.io/en/latest/?badge=latest
+    :alt: Documentation Status
+.. |License| image:: https://img.shields.io/pypi/l/jupyter_secrets.svg
+    :target: https://pypi.python.org/pypi/jupyter_secrets
+    :alt: License
+.. |Supported Python| image:: https://img.shields.io/pypi/pyversions/jupyter_secrets.svg
+    :target: https://pypi.python.org/pypi/jupyter_secrets
+    :alt: Supported Python Versions
+
+.. _API documentation: http://jupyter_secrets.readthedocs.io/en/latest/?badge=latest#module-jupyter_secrets
+
+.. |gif1| image:: ./docs/images/first-time.gif
+.. |gif2| image:: ./docs/images/next-time.gif
+
+.. _Colaboratory: https://colab.research.google.com/
+.. _Hydrogen: https://nteract.io/atom
+.. _Nteract: https://nteract.io
+.. _server-side services: https://12factor.net/
---
+++ jupyter_secrets.py
@@ -0,0 +1,115 @@
+"""This package provides functions for using secrets in a Jupyter notebook.
+
+These functions are for use in a notebook that needs to make use of secrets,
+such as passwords and API keys, to avoid storing the secret in the notebook
+source.
+
+.. note:: This package uses Keyring_. See the `Keyring API documentation`_ for
+    additional information about where secrets are stored, and how to change the
+    default location.
+
+.. _Keyring: https://pypi.python.org/pypi/keyring
+.. _Keyring API documentation: http://keyring.readthedocs.io/en/latest/?badge=latest
+"""
+
+
+__version__ = '0.1.0'
+
+import os
+import keyring
+try:
+    from IPython.display import clear_output
+except ImportError:
+    def clear_output(): pass
+
+
+def get_secret(service_name, username=None, *,
+               default=None, force_prompt=False, prompt=None):
+    """Read a secret from the keyring or the user.
+
+    Look for a secret in the keyring. If it's not present, prompt the user,
+    clear the cell, and save the secret.
+
+    Parameters
+    ----------
+    service_name : str
+        A keyring service name.
+
+    username : str, optional
+        A keyring username. This defaults to the value of the USER environment
+        variable. (Note that this can programmatically altered.)
+
+    default : str, optional
+        The default value, if the secret is not present in the keyring. If this
+        is non-None, the user is never prompted.
+
+    force_prompt : str, optional
+        If true, the user is always prompted for a secret.
+
+    prompt : str, optional
+        The text displayed to the user as part of the prompt.
+
+    Examples
+    --------
+    ::
+
+        from jupyter_secrets import *
+
+        TWILIO_API_KEY = get_secret('TWILIO_API_KEY')
+        TWILIO_API_KEY = get_secret('TWILIO_API_KEY', 'my-account')
+        TWILIO_API_KEY = get_secret('TWILIO_API_KEY', 'my-account', prompt="Enter the API key")
+    """
+    if username is None:
+        username = os.environ.get('USER')
+    password = None if force_prompt else keyring.get_password(service_name, username)
+    if password is None:
+        password = default
+    if password is None:
+        # uses ternary instead of `prompt or …`, in order to support prompt == ''
+        prompt = '{}[{}]'.format(service_name, username) if prompt is None else prompt
+        password = input(prompt)
+        keyring.set_password(service_name, username, password)
+        clear_output()
+    return password
+
+
+def set_secret(service_name, password, *, username=None):
+    """Sets a secret value.
+
+    Parameters
+    ----------
+    service_name : str A keyring service name.
+
+    password : str A keyring service name.
+
+    username : str, optional A keyring username. This defaults to the value of
+        the USER environment variable.
+
+    Notes
+    -----
+    The argument order to `set_secret` is different from
+    :func:`keyring.set_password`, and `username` can only be used as keyword
+    parameter. This is in order that `username` can be optional, for
+    compatibility with the more-frequently-used functions in this package.
+
+    """
+    if username is None:
+        username = os.environ.get('USER')
+    keyring.set_password(service_name, username, password)
+
+
+def delete_secret(service_name, username=None):
+    """Deletes a secret from the keyring.
+
+    Parameters
+    ----------
+    service_name : str
+        A keyring service name.
+
+    username : str, optional
+        A keyring username. This defaults to the value of the USER environment
+        variable.
+    """
+    if username is None:
+        username = os.environ.get('USER')
+    keyring.delete_password(service_name, username)
